d02d09085ddfb2526ed6e371a1dc9bd77627180a
kaif <mdkaif111967@gmail.com>
2025-04-09 21:50:13 +0530

    Build Basic calculator using java

---
+++ basic_calculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class basic_calculator{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter second number: ");
+        double num2 = sc.nextDouble();
+        System.out.println("Enter an operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+        double result;
+        switch(operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error! Division by zero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error! Invalid operator.");
+                return;
+        }
+        System.out.printf("%.1f %c %.1f = %.1f", num1, operator, num2, result);
+    }
+}
